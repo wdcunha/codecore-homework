4e4b8590242d7cc9e8dc5c78dbfa77abe950e3f4
Wellington <wdcunha@gmail.com>
2017-11-24 20:03:14 -0800

    I sending my homework Nov 24

---
+++ ruby-exercises-Nov-24/Question1/tester_include.rb
@@ -0,0 +1,40 @@
+require './helper_methods_titleize.rb'
+
+class TesterInclude
+  attr_accessor :str
+  include HelperMethods
+
+  def initialize
+    @str = "HELLO WORD"
+  end
+
+
+end
+
+
+
+
+# word.titleize
+#
+# b = Bird.new("B","yellow")
+# c = Cat.new("Tom","gray")
+# c.catch(b)
+# c.random_number("Tom","B")
+# c.hundred_times
+#
+# u      = User.new
+# u.name = "tam"
+# puts u.name_display
+# c      = Car.new
+# c.name = "toyota"
+# puts c.name_display
+
+# b1 = Bird.new "B"
+# c1 = Cat.new "C"
+# c1.catch b1
+#
+# for i in 1..100
+#   bi = Bird.new "B#{i}"
+#   ci = Cat.new "C#{i}"
+#   ci.catch bi
+# end
