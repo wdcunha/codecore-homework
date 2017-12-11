23faf7e8d72612e09ef572a213903c39f8bede89
Wellington <wdcunha@gmail.com>
2017-12-10 23:27:25 -0800

    some ammendmends trying to put random in teams

--- SuperTeamPicker/index.js
+++ SuperTeamPicker/index.js
@@ -7,8 +7,9 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/home', (request, response) => {
   response.render('home');
--- SuperTeamPicker/routes/cohorts.js
+++ SuperTeamPicker/routes/cohorts.js
@@ -16,6 +16,7 @@ router.get('/new', (request, response) => {
 // localhost:7000/cohorts/:id
 router.get('/:id', (request, response) => {
   const id = request.params.id;
+
   knex
     .first()
     .from('cohorts')
@@ -33,6 +34,50 @@ router.post('/', upload.single('picture'), (request, response) => {
 
   const filename = request.file.filename;
   const logo_url = path.join(UPLOADS_DIR, filename);
+
+  knex
+    .insert({name, members, logo_url})
+    .into('cohorts')
+    .returning('id')
+    .then(result => response.redirect('/cohorts'))
+    .catch(error => response.send(error));
+});
+
+// ////////
+router.post('/:id', (request, response) => {
+  const name = request.body.name;
+  const members = request.body.members;
+
+  function shuffleArray(array) {
+
+      for (var i = array.length - 1; i > 0; i--) {
+          var j = Math.floor(Math.random() * i);
+          var temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
+      }
+      return array;
+  }
+
+  function distributePlayers(names, numberOfTeams) {
+      var ret = {};
+      var teamCounter = 0;
+
+      for (var i = 0; i < names.length; ++i) {
+          if (!ret["Team" + (teamCounter + 1)]) {
+              ret["Team" + (teamCounter + 1)] = [];
+          }
+          ret["Team" + (teamCounter + 1)].push(names[i]);
+          if (++teamCounter == numberOfTeams) {
+              teamCounter = 0;
+          }
+      }
+      return ret;
+  }
+
+  var newarray =(shuffleArray(namesArr));
+  var arrays = distributePlayers(newarray, 3);
+  // console.log(arrays);
 
   knex
     .insert({name, members, logo_url})
