091606b26fe9871ec118d55c4a7b9b52bce01c89
Wellington <wdcunha@gmail.com>
2017-12-11 17:25:17 -0800

    working with returning data in show screen

--- SuperTeamPicker/routes/cohorts.js
+++ SuperTeamPicker/routes/cohorts.js
@@ -16,13 +16,53 @@ router.get('/new', (request, response) => {
 // localhost:7000/cohorts/:id
 router.get('/:id', (request, response) => {
   const id = request.params.id;
+  const members = request.query.members;
+  // const namesArr = members.replace(/\s+/gi, '').split(',');
+  const method = request.query.method;
+  const quantity = request.query.quantity;
+
+  function shuffleArray(array) {
+
+      for (var i = array.length - 1; i > 0; i--) {
+          var j = Math.floor(Math.random() * i);
+          var temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
+      }
+      return array;
+  }
+
+  function distributePlayers(names, numberOfTeams) {
+      var ret = {};
+      var teamCounter = 0;
+      for (var i = 0; i < names.length; ++i) {
+          if (!ret["Team" + (teamCounter + 1)]) {
+              ret["Team" + (teamCounter + 1)] = [];
+          }
+          ret["Team" + (teamCounter + 1)].push(names[i]);
+          if (++teamCounter == numberOfTeams) {
+              teamCounter = 0;
+          }
+      }
+      return ret;
+  }
+  let arrays = null;
+if (method === 'teamCount') {
+  let newarray =(shuffleArray(members.replace(/\s+/gi, '').split(',')));
+  arrays = distributePlayers(newarray, quantity);
+} else if (method === 'numberPerTeam') {
+  let newarray =(shuffleArray(members.replace(/\s+/gi, '').split(',')));
+  arrays = distributePlayers(newarray, quantity);
+}
+
+  // console.log(arrays);
 
   knex
     .first()
     .from('cohorts')
     .where({id})  //{id} === {id: id}
     .then(member => {
-      response.render('cohorts/show',{member});
+      response.render('cohorts/show',{arrays,member,method,members,quantity});
     })
     .catch(error => response.send(error));
 });
@@ -43,49 +83,6 @@ router.post('/', upload.single('picture'), (request, response) => {
     .catch(error => response.send(error));
 });
 
-// ////////
-router.post('/:id', (request, response) => {
-  const name = request.body.name;
-  const members = request.body.members;
-
-  function shuffleArray(array) {
-
-      for (var i = array.length - 1; i > 0; i--) {
-          var j = Math.floor(Math.random() * i);
-          var temp = array[i];
-          array[i] = array[j];
-          array[j] = temp;
-      }
-      return array;
-  }
-
-  function distributePlayers(names, numberOfTeams) {
-      var ret = {};
-      var teamCounter = 0;
-
-      for (var i = 0; i < names.length; ++i) {
-          if (!ret["Team" + (teamCounter + 1)]) {
-              ret["Team" + (teamCounter + 1)] = [];
-          }
-          ret["Team" + (teamCounter + 1)].push(names[i]);
-          if (++teamCounter == numberOfTeams) {
-              teamCounter = 0;
-          }
-      }
-      return ret;
-  }
-
-  var newarray =(shuffleArray(namesArr));
-  var arrays = distributePlayers(newarray, 3);
-  // console.log(arrays);
-
-  knex
-    .insert({name, members, logo_url})
-    .into('cohorts')
-    .returning('id')
-    .then(result => response.redirect('/cohorts'))
-    .catch(error => response.send(error));
-});
 
 // PATH: /cohorts VERB: GET List all the cohorts
 router.get('/', (request, response) => {
